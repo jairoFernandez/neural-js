f0516558053e8d542203765b60eefef80865ab60
Jairo Fernández <jairofg12@gmail.com>
2020-09-11 19:27:03 -0500

    initial

---
+++ example.js
@@ -0,0 +1,51 @@
+const Neuron = require('./neuron');
+
+const inputs = [new Neuron(), new Neuron()]; // Input Layer w/ 2 neurons
+const hiddens = [new Neuron(), new Neuron()]; // Hiddent Layer w/ 2 neurons
+const outputs = [new Neuron()];
+
+inputs[0].connect(hiddens[0]);
+inputs[0].connect(hiddens[1]);
+inputs[1].connect(hiddens[0]);
+inputs[1].connect(hiddens[1]);
+
+// Connect Hidden Layer to Output Layer
+hiddens[0].connect(outputs[0]);
+hiddens[1].connect(outputs[0]);
+
+const dataset = [
+    { inputs: [0,0], outputs: [0] },
+    { inputs: [0,1], outputs: [0] },
+    { inputs: [1,0], outputs: [0] },
+    { inputs: [1,1], outputs: [1] }
+];
+
+const activate = (input) => {
+    inputs.forEach((neuron, i) => neuron.activate(input[i]));
+    hiddens.forEach(neuron => neuron.activate());
+    return outputs.map(neuron => neuron.activate());
+  };
+
+const propagate = (target) => {
+    outputs.forEach((neuron, t) => neuron.propagate(target[t]));
+    hiddens.forEach(neuron => neuron.propagate());
+    return inputs.forEach(neuron => neuron.propagate());
+};
+
+const train = (iterations=1) => {
+    while(iterations > 0) {
+      dataset.map(datum => {
+        activate(datum.inputs);
+        propagate(datum.outputs);
+      });
+      iterations--;
+   }
+};
+
+train(10);
+
+// Test Network
+console.log(activate([0,0])); // ~0 (0.01214291222508886)
+console.log(activate([0,1])); // ~0 (0.08100696632854297)
+console.log(activate([1,0])); // ~0 (0.07793351045035582)
+console.log(activate([1,1])); // ~1 (0.8780115291725155)
